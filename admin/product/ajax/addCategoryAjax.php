2f6ea14da9d4bb44c57f84102334ab84e426f313
Deep <deepchhatralia77@gmail.com>
2022-01-26 09:11:14 +0530

    category image

--- admin/product/ajax/addCategoryAjax.php
+++ admin/product/ajax/addCategoryAjax.php
@@ -1,17 +1,62 @@
 <?php
 
-if (isset($_POST['category']) && $_POST['operation'] == 'add') {
-    include '../../../database.php';
-    $obj = new Database();
+include '../../../database.php';
+$obj = new Database();
 
-    $category = $_POST['category'];
+// Check if image file is a actual image or fake image
 
-    $result = $obj->insert('product_category', ['category_name' => $category]);
-
-    if ($result) {
-        echo 'Added';
+// this if condition is not working 
+if (isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if ($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
     } else {
-        // echo 'Please try again';
-        echo mysqli_error($obj->connection());
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+}
+
+$target_dir = "../category_uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+
+
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+} else {
+    // Check file size
+    if ($_FILES["fileToUpload"]["size"] > 5000000) {
+        echo "Sorry, your file is too large.";
+        $uploadOk = 0;
+    } else {
+        // Allow certain file formats
+        if (
+            $imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+            && $imageFileType != "gif"
+        ) {
+            echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+            $uploadOk = 0;
+        }
+    }
+}
+
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+    // if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo "The file " . htmlspecialchars(basename($_FILES["fileToUpload"]["name"])) . " has been uploaded.";
+
+        $category = $_POST['category'];
+
+        $result = $obj->insert('product_category', ['category_name' => $category, 'category_img' => htmlspecialchars(basename($_FILES["fileToUpload"]["name"]))]);
+    } else {
+        echo "<br/>Sorry, there was an error uploading your file.";
     }
 }
